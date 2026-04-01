6c0038cde48a4eeb6c457a5ffbd4d36b4e377b7d
Santiago-Buffaz <2415171@ucc.edu.ar>
2026-04-01 15:40:41 -0300

    configuracion 4

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb", // Esto permite subir logos y fotos de hasta 5 Megabytes
+    },
+  },
 };
 
 export default nextConfig;
